6f9e7242625c931a0b84b80fce0c8ea12cc3f9f8
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-27 21:51:02 +0530

    Date Picker project Starting boilerplate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,5 @@
-import { useRef } from 'react'
-import { CustomInput } from './customInput'
+import DatePicker from './DatePicker'
 
 export default function App() {
-  const inputRef = useRef()
-
-  function handleSubmit(e) {
-    e.preventDefault()
-    console.log(inputRef.current.value)
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <CustomInput ref={inputRef} />
-      <button type="submit">Submit</button>
-    </form>
-  )
+  return <DatePicker />
 }
-
-/*
- So we add ForwardRef there are some points to note
- 1. when we want to pass a ref to custom Component. then we need to choose forward ref to pass that ref
- 2. how to do.. like in our form there is custom Input component. which comes from external file customInput
- 3. that file has innerComponent which has our Input field, that we want to use in our function, so how can we pass the ref to that
- input
- 4. we need to choose forwardRef. we will do forwardRef(InnerFunction) than export that export const CustomInput .. wh
- 5. which we can use in our form and pass any props like title, type etc and ref.
- 6. important thing to not when creating the innerComponent, we need to pass the ref to input that we want to use , and that must
- be 2nd argument in that function after props
-*/
--- src/README.md
+++ src/README.md
@@ -1,9 +1,8 @@
-# Handling portals
+# Instructions
 
-portals what it do,, its about rendring the component from hirechrchy in which they load. we load that component in diffrent place in dom
+1. Create a `DatePicker` component that renders a button to open/close a calendar.
+   - This component should be a controlled component which means that it should accept an `onChange` and `value` prop (similar to an `input` element).
+   - This component should default to the current date when the page loads.
+   - When the calendar is opened it should open to the month of the selected date.
 
-so that useful we can render that in another div, another body , and its easy to setup..
-
-1. we are using reactPortal and rendrign the function element in document.body
-
-2. we in index.html called error-message, where this component will render
+The only other thing to note about this project is that it is built using `date-fns`. You can use any date library you want (even the built in JS Date API), but I will be using `date-fns` in my walkthrough video as I find it to be one of the best date libraries available.
