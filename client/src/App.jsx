d35a58b8e37b7bb1805aacb76105a5a41c57e71d
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-29 19:52:21 +0530

    Intersection Observer, Fetch images , fetching  of next images when last image loads. appending the list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,62 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { parseLinkHeader } from './parseLinkHeader'
 
 /*
- Below is boilerplate code for the photos which we are loading from backend, these
- are image with url and id info that are saving in photos array. and we are using that grid to
- show and load that images on dom to show
+ we are loading the fetchphotos function first time to load the images using useEffect
+ then ImageRef which is call back function attached to the last image of the fetch request which is on dom
+ when intersection observer run that will fetch the fetchPhotos(nextLinkRef.current) means we are fetching the
+ next batch of photoes to load on page. here the nextLinkRef.current is coming from the previous fetch request headers 
+ if you check the headers for request you will see the Links that have first, next, last links. that we converted using the
+ function parseLinkHeader "these links into object".
+ that next link is used by observer for fetching the next batch of images
 */
+
 export default function App() {
   const [photos, setPhotos] = useState([])
+  const nextLinkRef = useRef()
+  async function fetchPhotos(URL, { overWrite = false } = {}) {
+    const res = await fetch(URL)
+    nextLinkRef.current = parseLinkHeader(res.headers.get('Link')).next
+
+    const images = await res.json()
+    if (overWrite) {
+      setPhotos(images)
+    } else {
+      setPhotos(prevPhotoes => {
+        return [...prevPhotoes, ...images]
+      })
+    }
+  }
+
+  const ImageRef = useCallback(image => {
+    if (image == null) return
+
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        //Todo Load next elements
+        fetchPhotos(nextLinkRef.current)
+        observer.unobserve(image)
+      }
+    })
+    observer.observe(image)
+  }, [])
+
   useEffect(() => {
-    fetch('http://127.0.0.1:3000/photos?_page=1&_limit=10')
-      .then(res => res.json())
-      .then(setPhotos)
+    fetchPhotos('http://127.0.0.1:3000/photos?_page=1&_limit=10', {
+      overWrite: true
+    })
   }, [])
 
   return (
     <div className="grid">
-      {photos.map(photo => {
-        return <img src={photo.url} key={photo.id} />
+      {photos.map((photo, index) => {
+        return (
+          <img
+            src={photo.url}
+            key={photo.id}
+            ref={index === photos.length - 1 ? ImageRef : undefined}
+          />
+        )
       })}
     </div>
   )
