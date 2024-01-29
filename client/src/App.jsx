f708ded60dbc1bd9679acd0456b01d1abdf846c4
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-29 22:29:33 +0530

    Loading Skelton on each fetch request, final code for Infinite Scroll Image .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,34 +2,43 @@ import { useCallback, useEffect, useRef, useState } from 'react'
 import { parseLinkHeader } from './parseLinkHeader'
 
 /*
- we are loading the fetchphotos function first time to load the images using useEffect
- then ImageRef which is call back function attached to the last image of the fetch request which is on dom
- when intersection observer run that will fetch the fetchPhotos(nextLinkRef.current) means we are fetching the
- next batch of photoes to load on page. here the nextLinkRef.current is coming from the previous fetch request headers 
- if you check the headers for request you will see the Links that have first, next, last links. that we converted using the
- function parseLinkHeader "these links into object".
- that next link is used by observer for fetching the next batch of images
-*/
+ Added Loading skelton when fetch request goes, we added 2 sec delay for each fetch request that goes
+ we created the isLoading variable , that will load skeleton,, for creating skelton we use array.from to create
+ sequence of numbers from array using length property, and the limit is equal to api that we are using in api request..
+ last thing to note, like in our request we have total 100 images, so after that if fetch request goes the nextLinkRef will comes 
+ null. so in that case we also added condition in useCallback to check if that next link is null then we will return
 
+
+*/
+const LIMIT = 50
 export default function App() {
   const [photos, setPhotos] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
   const nextLinkRef = useRef()
   async function fetchPhotos(URL, { overWrite = false } = {}) {
-    const res = await fetch(URL)
-    nextLinkRef.current = parseLinkHeader(res.headers.get('Link')).next
-
-    const images = await res.json()
-    if (overWrite) {
-      setPhotos(images)
-    } else {
-      setPhotos(prevPhotoes => {
-        return [...prevPhotoes, ...images]
-      })
+    setIsLoading(true)
+    try {
+      await new Promise(res => setTimeout(res, 2000))
+      const res = await fetch(URL)
+      nextLinkRef.current = parseLinkHeader(res.headers.get('Link')).next
+      const images = await res.json()
+      if (overWrite) {
+        setPhotos(images)
+      } else {
+        setPhotos(prevPhotoes => {
+          return [...prevPhotoes, ...images]
+        })
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
   const ImageRef = useCallback(image => {
-    if (image == null) return
+    if (image == null || nextLinkRef.current == null) return
 
     const observer = new IntersectionObserver(entries => {
       if (entries[0].isIntersecting) {
@@ -42,9 +51,12 @@ export default function App() {
   }, [])
 
   useEffect(() => {
-    fetchPhotos('http://127.0.0.1:3000/photos?_page=1&_limit=10', {
-      overWrite: true
-    })
+    fetchPhotos(
+      `http://127.0.0.1:3000/photos-short-list?_page=1&_limit=${LIMIT}`,
+      {
+        overWrite: true
+      }
+    )
   }, [])
 
   return (
@@ -58,6 +70,14 @@ export default function App() {
           />
         )
       })}
+      {isLoading &&
+        Array.from({ length: LIMIT }, (_, index) => index).map(n => {
+          return (
+            <div className="skeleton" key={n}>
+              Loading...
+            </div>
+          )
+        })}
     </div>
   )
 }
