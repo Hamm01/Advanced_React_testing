e5ede0ecd6b7e643c992d4a922a53fb61f1cf3fb
Himanish kochhar <himanish.kochhar@gmail.com>
2024-02-04 21:31:31 +0530

    Two ways for creating Types for props

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import Child from "./Child"
+import Child1 from "./Child"
 
 
 function App() {
 
   return (<>
     <h1>Typescript initialization</h1>
-    <Child name="Typescript intialized using child" />
+    <Child1 name="Typescript intialized using child" />
   </>)
 }
 
--- client/src/Child.tsx
+++ client/src/Child.tsx
@@ -1,5 +1,21 @@
 
-export default function Child({ name }: { name: string }) {
+type ChildType = {
+    name: string
+}
+
+// Below are two ways how can set types for props first is using type  assigning directly to props in function
+
+// next function we need to pass as React.Fc, in which we pass the type as generic
+
+
+export default function Child({ name }: ChildType) {
+    return (
+        <h1>{name}</h1>
+    )
+}
+
+
+export const Child1: React.FC<ChildType> = ({ name }) => {
     return (
         <h1>{name}</h1>
     )
