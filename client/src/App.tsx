ac77373889d4fa4385f4aa6b23ac6a13e081cdd1
Himanish kochhar <himanish.kochhar@gmail.com>
2024-02-04 23:57:13 +0530

    Children using prop and declaring type ad ReactNode becuse to pass div as children inside child component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import Child from "./Child"
-import Child1 from "./Child"
+import { Child } from "./Child"
 
 
 function App() {
 
-  return (<>
-    <h1>Typescript initialization</h1>
-    <Child1 name="Typescript intialized using child" />
-  </>)
+  return (
+    <>
+      <h1>Typescript initialization</h1>
+      <Child>
+        <div> Child using children prop</div>
+      </Child>
+    </>)
 }
 
 export default App
--- client/src/Child.tsx
+++ client/src/Child.tsx
@@ -1,22 +1,10 @@
+import type { ReactNode } from 'react'
 
-type ChildType = {
-    name: string
+
+type ChildProps = {
+    children: ReactNode
 }
 
-// Below are two ways how can set types for props first is using type  assigning directly to props in function
-
-// next function we need to pass as React.Fc, in which we pass the type as generic
-
-
-export default function Child({ name }: ChildType) {
-    return (
-        <h1>{name}</h1>
-    )
-}
-
-
-export const Child1: React.FC<ChildType> = ({ name }) => {
-    return (
-        <h1>{name}</h1>
-    )
+export function Child({ children }: ChildProps) {
+    return <h1>{children}</h1>
 }
