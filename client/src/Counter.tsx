e630dc94387a1919490005eb6d10166caa9343a4
Himanish kochhar <himanish.kochhar@gmail.com>
2024-02-22 17:02:04 +0530

    shouldComponentUpdate function to check the prevprop and prev state to make class component rerender or not

--- client/src/Counter.tsx
+++ client/src/Counter.tsx
@@ -5,6 +5,15 @@ export class Counter extends PureComponent {
         super(props)
         this.state = { count: props.initialCount }
     }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return false
+        // In the class componet we can use this function to check the nextprops and next state to make the component
+        // rerenders which if we return true this will make component rerenders or in false it will not,,
+        // but in case of class component we need to must check that class component not rerenders unlike functional components
+        // when our state changes.. so we need to take care with nexprops and nextstate to make return true or false , true for
+        // to render that component again or false for not        
+    }
     render() {
         return (
             <>
